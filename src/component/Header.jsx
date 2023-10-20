ac2639668d17a28be41fdadd75c20685f857420e
harrshsaini <harshmsd95@gmail.com>
2023-10-20 15:30:45 +0530

    project done

---
+++ src/component/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ToolBar from '@mui/material/Toolbar';
+import { AppBar } from '@mui/material';
+
+
+const Header = () =>{
+    return (
+        <AppBar sx={{bgcolor: "#060606", height: "9vh"}} position="static">
+            <ToolBar>
+                 
+            </ToolBar>
+        </AppBar>
+    )
+}
+export default Header;
---
+++ src/component/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Code from "./Codee";
+import Result from "./Result";
+const Home = () =>{
+    return (
+        <>
+          <Header/>
+          <Code/>
+          <Result/>
+        </>
+    )
+}
+export default Home;
